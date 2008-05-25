909fa96bf4ea1ecb3cf7586a1a224dbe314fb725
guilhem.duche <guilhem.duche@ae48ae66-6a45-0410-b38e-211266189506>
2008-05-25 21:13:53 +0000

    wiiuseJ 0.12 work in progress.

---
+++ trunk/WiiUseJ/src/wiiusej/utils/NunchukJoystickEventPanel.java
@@ -0,0 +1,36 @@
+/**
+ * This file is part of WiiuseJ.
+ *
+ *  WiiuseJ is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WiiuseJ is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with WiiuseJ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package wiiusej.utils;
+
+import wiiusej.wiiusejevents.physicalevents.ExpansionEvent;
+import wiiusej.wiiusejevents.physicalevents.JoystickEvent;
+import wiiusej.wiiusejevents.physicalevents.NunchukEvent;
+
+/**
+ * Panel to display nunchuk joystick events.
+ * @author guiguito
+ */
+public class NunchukJoystickEventPanel extends JoystickEventPanel {
+
+    @Override
+    public JoystickEvent getJoystikEvent(ExpansionEvent e) {
+        if (e instanceof NunchukEvent) {
+            return ((NunchukEvent) e).getNunchukJoystickEvent();
+        }
+        return null;
+    }
+}
